d4744fabebe509041290ef27d609da7d380680be
bryantt23 <bryantt23@gmail.com>
2015-04-23 22:54:48 -0700

    add failing tests for titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Bryant's Webpage"
   end
 
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Bryant's Webpage"
   end
 
   test "should get mini_goals" do
     get :mini_goals
     assert_response :success
+    assert_select "title", "Mini Goals | Bryant's Webpage"
   end
 
 end
