85209d411d1c808e59f1ae00792d758f1d316319
bryantt23 <bryantt23@gmail.com>
2015-04-25 01:13:46 -0700

    got dog image to rotate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,47 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jQueryRotate
 //= require_tree .
+
+$(function(){
+  // $("#img").rotate(-7);
+
+
+
+
+  // $("#img").rotate({
+  //    bind:
+  //      {
+  //         mouseover : function() {
+  //             $(this).rotate({animateTo:-6})
+  //         },
+  //         mouseout : function() {
+  //             $(this).rotate({animateTo:0})
+  //         }
+  //      }
+  // });
+
+  var angle = 0;
+  setInterval(function(){
+        angle+=.5;
+       $("#rotateContainer").rotate(angle);
+  },50);
+
+
+
+  // var value = 0
+  // $("#image").rotate({
+  //    bind:
+  //      {
+  //         click: function(){
+  //             value +=90;
+  //             $(this).rotate({ animateTo:value})
+  //         }
+  //      }
+  //
+  // });
+
+
+
+});
