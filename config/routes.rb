4905e0dbbee2d1e30e1152cf2b0f4ed3c806500d
bryantt23 <bryantt23@gmail.com>
2015-04-26 02:52:19 -0700

    test passes, fixed incorrect route from previous commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   root             'static_pages#home'
+
   get 'mini_goals'    => 'static_pages#mini_goals'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'contact' => 'static_pages#timer'
+  get 'timer' => 'static_pages#timer'
 
 
 end
