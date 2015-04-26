f0e8e0211427b3fe4d0c0d288de795f5e7aea623
bryantt23 <bryantt23@gmail.com>
2015-04-26 01:33:55 -0700

    dog pic now fades in and out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,63 +19,33 @@
 
 
 
-// $(window).load(function() {
-  // $(window).onload = function(){/*your code*/
-  // $(document).ready(function(){
-  // $(document).ready( function() {
-    // $(window).load( function() {
-
-    // original code
 $(function(){
 
-// // this makes the dog image rotate continuously
-//   var angle = 0;
-//   setInterval(function(){
-//         angle+=.5;
-//        $("#no_idea_dog").rotate(angle);
-//   },50);
-
-
-
-
-  // $("#click-22").click(function () {
-  //   $("#22").fadeIn("slow");
-  // });
-  // $("#set-22").click(function () {
-  //   $("#22").fadeOut("slow");
+  //   $("#no_idea_dog").rotate({
+  //    bind:
+  //      {
+  //         mouseover : function() {
+  //             $(this).rotate({animateTo:-90})
+  //         },
+  //         mouseout : function() {
+  //             $(this).rotate({animateTo:0})
+  //         }
+  //      }
+  //
   // });
 
 
-    $("#no_idea_dog").rotate({
-     bind:
-       {
-          mouseover : function() {
-              $(this).rotate({animateTo:-90})
-          },
-          mouseout : function() {
-              $(this).rotate({animateTo:0})
-          }
-       }
+  function fadeIn() {
+      jQuery("#no_idea_dog").fadeIn(5000);
+      fadeOut();
+  }
 
-  });
+  function fadeOut() {
+      jQuery("#no_idea_dog").fadeOut(5000);
+      fadeIn();
+  }
 
-
-// $(window).load(function() {
-  // function fadeIn(window.load) {
-  // window.onload = function fadeIn() {/*your code*/
-
-
-  // function fadeIn() {
-  //     jQuery("#no_idea_dog").fadeIn(5000);
-  //     fadeOut();
-  // }
-  //
-  // function fadeOut() {
-  //     jQuery("#no_idea_dog").fadeOut(5000);
-  //     fadeIn();
-  // }
-  //
-  // fadeOut();
+  fadeOut();
 
 
 });
